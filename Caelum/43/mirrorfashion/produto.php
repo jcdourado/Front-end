962b741b8b40bb01b603b4012d1a6f2fae13ebd9
Julio Cezar Dourado <jcd17071997@hotmail.com>
2016-02-05 11:15:08 -0200

    Commit final capitulo 8

--- Caelum/43/mirrorfashion/produto.php
+++ Caelum/43/mirrorfashion/produto.php
@@ -4,6 +4,7 @@
         $cabecalho_css = '<link rel="stylesheet" href="css/produto.css">'; 
         include("cabecalho.php");
       ?>
+  <div class="produto-back">
     <div class="container">
       <div class="produto">
         <h1>Fuzzy Cardigan</h1>
@@ -30,11 +31,55 @@
                 
             </fieldset>
 
+            <fieldset class="tamanhos">
+              <legend>Escolha o tamanho</legend>
+              <input type="range" min="36" max="46" step="2" name="tamanho"
+              id ="tamanho">
+
+            </fieldset>
+
             <input type="submit" class="comprar" value="Comprar">
         </form>
       </div>
+
+      <div class="detalhes">
+        <h2>Detalhes do produto</h2>
+
+        <p>Esse é o melhor casaco de Cardigã que você já viu. Excelente 
+        material italiano com estampa desenhada pelos artesãos da 
+        comunidade de Krotor nas ilhas gregas. Compre já e receba hoje 
+        mesmo pela nossa entrega a jato.</p>
+
+        <table>
+        <thead>
+          <tr>
+            <th>Característica</th>
+            <th>Detalhe</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Modelo</td>
+            <td>Cardigã 7845</td>
+          </tr>
+          <tr>
+            <td>Material</td>
+            <td>Algodão e poliester</td>
+          </tr>
+          <tr>
+            <td>Cores</td>
+            <td>Azul, Rosa e Verde</td>
+          </tr>
+          <tr>
+            <td>Lavagem</td>
+            <td>Lavar a mão</td>
+          </tr>
+        </tbody>
+      </table>
+      </div>
     </div>
-      <?php
+   </div>
+     <?php
         include("rodape.php");
       ?>
 
