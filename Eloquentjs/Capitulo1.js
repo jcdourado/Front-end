984374afc2f8c9c14fb3c2e7831e3d3625713415
Julio Cezar Dourado <jcd17071997@hotmail.com>
2016-02-19 13:31:36 -0200

    Commit inicial JS com eloquente js

---
+++ Eloquentjs/Capitulo1.js
@@ -0,0 +1,9 @@
+/* Javascript - criado em 1995 pela NetScape e para ela
+	
+	Depois todos os navegadores graficos o inseriram
+	
+	Nada a ver com o Java, apenas o nome
+
+	Também chamado de ECMAScript, para diferenciar, a versão 3 já possui em todos os navegadores, a atual é a 5º
+
+	*/
