626c1fbed3628b114cb4b304beef037d19e7689c
Julio Cezar Dourado <jcd17071997@hotmail.com>
2016-02-22 11:36:05 -0300

    Commit final capitulo 1 eloquentjs

--- Eloquentjs/Capitulo1.js
+++ Eloquentjs/Capitulo1.js
@@ -6,4 +6,27 @@
 
 	Também chamado de ECMAScript, para diferenciar, a versão 3 já possui em todos os navegadores, a atual é a 5º
 
-	*/
+	Tipos de variaveis:
+		number, string, boolean, object, function, undefined
+
+	64 bits pra number
+	+,-,*,/,%
+	Tipos especiais de number: Infinity, -Infinity e NaN (quando dá erro matematico)
+
+	typeof para pegar qual o tipo da var (sem parenteses)
+
+	Só existe um valor no JS que nao é igual a ele mesmo: NaN
+
+	null e undefined
+
+	Tem conversao em tempo de execução.
+
+	Strings vazias, 0 e NaN -> false
+
+	Há o operador === ou !== que testa o tipo juntamente.
+
+	|| && - curto circuito (impede o outro lado)
+
+	
+
+		*/
