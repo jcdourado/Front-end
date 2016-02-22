da28c872bb7211ebd2536a24d7a6bc2eadddb626
Julio Cezar Dourado <jcd17071997@hotmail.com>
2016-02-22 12:48:18 -0300

    Commit final, capitulo 3

---
+++ Eloquentjs/Capitulo2.js
@@ -0,0 +1,17 @@
+/*
+	Sempre que possivel, colocar o ";"
+
+	O "+" vai atribuir a string junto com o valor, virando outra string,
+	ja em casos com -,*, e etc. ele vai tranformar a string em num e fazer a conta
+
+	Declarar variaveis: var nome
+
+	undefined: variavel nao definida (sem valor)
+
+	Usa-se "," ou "+" para concatenar variaveis
+
+	alert, confirm, prompt(o segundo parametro é o valor default)
+
+	Para converter um valor: Number() - caso nao consiga, retorna NaN
+
+*/
