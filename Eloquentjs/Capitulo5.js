8d4167a9c44fe0835e89ef93ebce4b3ce8e27d91
Julio Cezar Dourado <jcd17071997@hotmail.com>
2016-02-25 13:06:43 -0300

    Commit final cap 5

--- Eloquentjs/Capitulo5.js
+++ Eloquentjs/Capitulo5.js
@@ -1,7 +1,11 @@
 /*
 	Funcoes de ordem superior: funcoes que operam em outras funcoes
 
-	
+	JSON: armazenamento de dados e formato de comunicação
+		Se escreve igual arrays e objetos em JS
+		Não chama funcoes nem tem comentarios, nem calculos
 
+	JSON.stringify: converte em string em JSON
+	JSON.parse: converter para JSON mesmo
 
 */
