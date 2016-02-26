88d3b0cb1b6d330351615f4df6cf2678cbc24f1c
Julio Cezar Dourado <jcd17071997@hotmail.com>
2016-02-26 11:51:05 -0300

    Commit final cap 8

--- Eloquentjs/Capitulo8.js
+++ Eloquentjs/Capitulo8.js
@@ -3,6 +3,12 @@
 
 	O JS cria variavel global quando nao se declara com o "var"
 
-	throw new - try catch 
+	throw new Error("")- try catch finally
+
+	Para criar um novo tipo de exception;
+		"erro".prototype = Object.create(Error.prototype)
+
+	Assertions:
+		
 
 */
