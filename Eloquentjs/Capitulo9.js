ebb38fcb0806993e97d72c62f214a1719bcd3f4e
Julio Cezar Dourado <jcd17071997@hotmail.com>
2016-02-26 12:11:14 -0300

    Commit final cap 9

---
+++ Eloquentjs/Capitulo9.js
@@ -0,0 +1,13 @@
+/*
+	Expressao regular é um objeto, criado por RegExp ou um valor literal(entre barras)
+
+	metodos: 
+		test
+
+
+	Date: conta a partir de 0
+		new - cria com a data atual
+		getTime: pega desde 1970
+		(ano,mes,dia,hora,minuto,seg,miliseg)
+
+*/
