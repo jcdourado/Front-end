865e50270f03cab99b85e1316f1f111ed0f9c65e
Julio Cezar Dourado <jcd17071997@hotmail.com>
2016-02-26 13:21:55 -0300

    Commit final cap 13

---
+++ Eloquentjs/Capitulo13.js
@@ -0,0 +1,9 @@
+/*
+	DOM: Representação do documento - Document Object Model
+	Para cada caixa HTML podemos interagir: pegar textos, tags
+
+	document: var global
+		documentElement: se refere à tag html
+
+
+*/
