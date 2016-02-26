b1b23e561518deb81f8d16e7d9a4745024556cba
Julio Cezar Dourado <jcd17071997@hotmail.com>
2016-02-26 09:35:24 -0300

    Commit final cap6

--- Eloquentjs/Capitulo6.js
+++ Eloquentjs/Capitulo6.js
@@ -1,5 +1,20 @@
 /*
 	Prototypes: outro objeto que é usado como fonte de fallback para as propriedades
 	Object.prototype: ancestral de todos os prototypes
+	
+	Arrays derivam de Array.prototype
+	Function de Function.prototype
+
+	Object.getPrototypeOf
+	Object.create - criar um prototype especifico
+	Object.keys - retorna um array com nomes de propriedades 
+
+	Construtores sao criados como qualquer outra funcao - chama pelo new
+	
+	"Classe".prototype."metodo" - adicionar um metodo
+
+	instanceof
+
+	
 
 */
