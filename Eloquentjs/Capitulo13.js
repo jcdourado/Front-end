dd9d924ca516b104eccd9457612c6491fadae77d
Julio Cezar Dourado <jcd17071997@hotmail.com>
2016-02-29 11:03:22 -0300

    Commit final cap13

--- Eloquentjs/Capitulo13.js
+++ Eloquentjs/Capitulo13.js
@@ -5,5 +5,42 @@
 	document: var global
 		documentElement: se refere à tag html
 
+	document.documentElement -> raiz do documento
+	
+	Cada nó tem a propriedade nodeType:
+		1 - ELEMENT_NODE
+		3 - TEXT_NODE
+		8 - COMMENT_NODE
+
+	Todo no tem:
+		parentNode, firstChild, lastChild, previousSibling, nextSibling
+			// Dá null caso nao tenha!
+		nodeValue: texto do nó
+		getElementsByTagName, getElementById, getElementsByClassName
+		removeChild, appendChild, insertBefore, replaceChild
+		createTextNode, createElement
+		getAttribute, setAttribute
+		offsetWidth, offsetHeight: altura e largura em pixels
+		clientWidth, clientHeight: espaço dentro, sem contar a borda
+		getBoundingClientRect: pegar a posicao
+		pageXOffset, pageYOffset: X,Y na tela
+	
+	Alterar por estilo:
+		style - acessar os elementos de estilo
+
+	querySelectorAll e querySelector
+
+	requestAnimationFrame - solicitar ao navegador para fazer a animação
+	
+
+	
+	Comparar tipos:
+		type of -> retorna object (mesmo sendo um outro objeto), ou array, ou string, ou integer
+		.constructor -> retorna o construtor mesmo
+
+	Variaveis de escopo global:
+		Pode ser sem o "var"
+		Definifidas dentros de ifs, etc
+		Em funcoes, elas viram locais, a nao ser sem o "var"
 
 */
