fe21f72f09354fac7058d85694e6ebeeb5a7faa4
Julio Cezar Dourado <jcd17071997@hotmail.com>
2016-02-29 13:06:14 -0300

    Commit capitulo 14 final

---
+++ Eloquentjs/Capitulo14.js
@@ -0,0 +1,34 @@
+/*
+	addEventListener("evento",funcao) - pode por varios em um elemento
+	
+	onclick(function) - só pode um por elemento
+	
+	removeEventListener
+	
+	stopPropagation - fazer com que o evento nao se propague para os outros
+
+	preventDefault - prevenir o comportamento normal
+
+	mousedown, keydown, mouseup, keyup, mousemove - sao clicados!
+
+	"String".charCodeAt(posicao) - pegar o unicode ali
+
+	shiftKey, ctrlKey, altKey e metakey(command)
+
+	click, dlbclick
+
+	mousemove, mouseover, mouseout, scroll
+
+	focus, blur(sai)
+
+	window.load - qnd termina de carregar, beforeunload - qnd ta saindo
+
+	setTimeout- tempo para executar uma função
+	clearTimeout - limpar o tempo = cancelAnimationFrame x requestAnimationFrame
+	
+	setInterval x clearInterval
+
+	String.fromCharCode(event.charCode)
+
+
+*/
