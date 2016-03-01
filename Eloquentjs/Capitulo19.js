57132b12775c1009e782132855c22d1dcef4f04b
Julio Cezar Dourado <jcd17071997@hotmail.com>
2016-03-01 13:17:45 -0300

    Commit final capitulo 19

---
+++ Eloquentjs/Capitulo19.js
@@ -0,0 +1,24 @@
+/*
+	document.activeElement - elemento atualmente focado
+	getElementsByName 
+
+	FileReader: ler arquivos:
+		.result: resultado
+		.readAsText: como txto
+
+	Elemento html:
+		tabindex: indica quanto ele deve pular no tab
+		disabled: desativa
+		form - elements: elementos
+			value.value: valor do elemento
+		event.target.value : valor do evento
+		select: .options: opcoes do select
+				option.selected: selecionado
+		file: .size, .type, .name
+
+	localStorage: armazena dados até que o user apague o historico
+		setItem, removeItem
+
+	sessionStorage: armazena até o fim da sessao
+
+*/
