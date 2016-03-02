2f3327cc04a0344d9e5b8267e6d12be7dec897b5
Julio Cezar Dourado <jcd17071997@hotmail.com>
2016-03-02 12:01:56 -0300

    Commit final livro Eloquent - falta Node.js

--- Eloquentjs/Capitulo9.js
+++ Eloquentjs/Capitulo9.js
@@ -10,4 +10,26 @@
 		getTime: pega desde 1970
 		(ano,mes,dia,hora,minuto,seg,miliseg)
 
+	Nao pode usar o new com o Math
+
+	JS nao tem classes, apenas objetos
+	Tudo herda de objeto
+	Trabalha atraves de heranca de prototipos
+
+	Pode-se chamar o construtor de outra classe atraves do call dentro do construtor
+
+	Object.create - cria uma copia
+
+	Object.defineProperty(objeto, 'atributo', { implementacao}):
+	
+		Object.defineProperty(cachorro, 'idade', {
+		  value: 1,
+		  get: funtion() {
+		    // já que cada ano de um cachorro é equivalente a 7 anos nossos
+		    return idade * 7
+		  },
+		  set: function(value) {
+		    idade = value
+		  }
+		})
 */
---
+++ Eloquentjs/W3Scholl1.js
@@ -0,0 +1,18 @@
+/*
+	<script src="local.js"> </script>
+
+	Da pra criar funcoes como: new Function(args,retorno)
+
+	call x apply : apply pega os parametros como um array
+
+	Output:
+		.innerHTML = "" -> só pode ser feito pelo document.getElementById
+		document.write("")
+
+		
+
+
+	
+	
+
+*/
