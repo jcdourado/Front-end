fd0ce494252723b76a8b18f0ad159e7d0be342dd
Julio Cezar Dourado <jcd17071997@hotmail.com>
2016-03-02 13:14:17 -0300

    Commit livro Pro JS techiniques

---
+++ FrameworksJSParaObjetos.js
@@ -0,0 +1,13 @@
+/*
+	Base : Orientacao a objetos
+	Prototype : Orientacao a objetos
+	Dojo: name-space(complete web-devel)
+	YUI:  name-space(complete web-devel)
+	JSMin: minimificador
+	Packer: minimificador mais preciso
+	JSAN: distribuir dependencias (libraries)
+
+
+
+
+*/
