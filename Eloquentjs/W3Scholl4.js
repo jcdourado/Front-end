828cbad94d92343706209a9c9e72bd766ab95798
Julio Cezar Dourado <jcd17071997@hotmail.com>
2016-03-04 10:17:07 -0300

    Commit pré-DOM

---
+++ Eloquentjs/W3Scholl4.js
@@ -0,0 +1,14 @@
+/*
+	Elementos que ajudam na verificação:
+		HTML:
+			required, max, min, disabled
+		CSS:
+			:valid,:disabled,:invalid,:required,:optional
+
+	CSS tem a propriedade ::selection que altera quando um texto é selecionado
+
+	
+
+
+
+*/
