8d92ccf87b27e6a706aaa68faf1bc29c933698ae
Julio Cezar Dourado <jcd17071997@hotmail.com>
2016-03-04 13:03:08 -0300

    Commit BOM

---
+++ Eloquentjs/BOM.js
@@ -0,0 +1,14 @@
+/*	
+	window -> representa o browser
+	TUDO DO DOM É PROPRIEDADE DO WINDOW
+	
+	metodos:
+		close, open, resizeTo, innerHeight, innerWidth
+
+	location.href, protocol, hostname
+
+	history.back e foward
+
+
+
+*/
