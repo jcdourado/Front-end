be7cca460c047e482f16a2c48af62e6f36bb0132
Julio Cezar Dourado <jcd17071997@hotmail.com>
2016-03-17 11:52:14 -0300

    Commit for of

--- MDNjs/MDN.js
+++ MDNjs/MDN.js
@@ -8,8 +8,17 @@
 
 	Promise(resolve, reject) - chama um promise: caso sucesso chama resolve, caso falha chama reject
 
+	for in(pega os indices) x for of(pega os valores internamente)
 
+	arguments.calle() para recursao
 
+	function (a,b, ...args) -> Rest parameter, igual ao arguments
 
+	Lambda:
+		function a(s => s.length) - exemplo
+
+	eval() e uneval() - converte string x object
+
+	
 
 */
