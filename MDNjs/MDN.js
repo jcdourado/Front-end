759e95f0a38ecde0909c45cdbe96bc3bd323f688
Julio Cezar Dourado <jcd17071997@hotmail.com>
2016-03-17 09:50:37 -0300

    Commit inicial MDN.js

---
+++ MDNjs/MDN.js
@@ -0,0 +1,15 @@
+/*
+	var
+	const, let -> escopo de funcão
+
+	Primitivo(false em comparação) x Boolean object(true)
+
+	throw e throw new
+
+	Promise(resolve, reject) - chama um promise: caso sucesso chama resolve, caso falha chama reject
+
+
+
+
+
+*/
